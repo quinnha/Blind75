a4f704a98ddea2e78144a7d3e09e081a12008c7b
quinnha <cadetha@gmail.com>
2022-08-13 13:39:02 -0400

    Added Group Anagrams

--- Extra/3 - Group Anagrams.py
+++ Extra/3 - Group Anagrams.py
@@ -1,7 +1,21 @@
-
+# create dic of L-value = sorted_str - str, and append to dic if exists
 def groupAnagrams(strs):
-    return 0
 
-x=  "hjello"
+    out = []
+    d = {}
+    
+    for i in strs:
+        str_s = "".join(sorted(i))
+        if not (str_s in d):  
+            d[str_s] = [i] 
+        else:
+            d[str_s].append(i)
+    
+    for key in d:
+        out.append(d[key])
 
-print("".join(sorted(x)))
+    return out
+
+
+print (groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+print (groupAnagrams(["a"]))
