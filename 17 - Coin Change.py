2632f1766f86fb2f0f44e11e008d6115c0975722
quinnha <cadetha@gmail.com>
2022-08-14 22:12:09 -0400

    Added Coin Change

--- 17 - Coin Change.py
+++ 17 - Coin Change.py
@@ -1,31 +1,25 @@
-from itertools import count
 
+# dp[0] = 1, dp[1] = 1 + dp[0], dp[2] = 1 + dp[1], counting min num coins for any i, so work from 1 up to amount
 
 def coinChange(coins,amount):
 
-    if amount == 0: return 0
+    dp = [amount + 1] * (amount + 1) # set array to maximum amount, this stores min no. of coins needed at any amount
 
-    coins.sort()
-    n = 0
+    dp[0] = 0 # set base case
 
-    if amount in coins:
-        return 1
-    else: 
-        if len(coins) == 1:
-            if coins[0] < amount: 
-                n = coins[0]
-        for i in range(len(coins)-1,-1,-1):
-            if coins[-i] < amount:
-                n = coins[i]
-                break
+    for i in range(1, amount + 1): #loop through all in dp[]
+        for coin in coins:
+            if i-coin >= 0: #if the coin is less than the amount looked at (if its bigger, then it wont fit)
+                dp[i] = min(dp[i], 1 + dp[i-coin]) # find the min of all attemps (7 - 3+4, 1+6, 2+5, etc)
 
-    if n == 0:        
-        return -1
+    return dp[amount] if dp[amount] != amount + 1 else -1 # if there has been no change, then return -1
+    
 
-    return 1 + coinChange(coins, amount-n)
+
     
 
 
 
-print(coinChange([1,2,5],11))
-print(coinChange([1,21474836472],2))
+# print(coinChange([2,5],11))
+# print(coinChange([1,21474836472],2))
+print(coinChange([186,419,83,408], 6249))
