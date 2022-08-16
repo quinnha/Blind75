9629dbc69c99b824a103e0641ea40491dc95c609
quinnha <cadetha@gmail.com>
2022-08-15 20:00:41 -0400

    Added Longest Common Subsequence

---
+++ 19 - Longest Common Subsequence.py
@@ -0,0 +1,14 @@
+# bottom up, see if char marches, and then add 1 to the next ones, else (i dont know)
+def longestCommonSubsequence(text1, text2):
+
+    n,m=len(text1),len(text2)
+    dp=[[0]*(m+1) for _ in range(n+1)]
+    for i in range(n):
+        for j in range(m):
+            if text1[i]==text2[j]:
+                dp[i+1][j+1]=dp[i][j]+1
+            else:
+                dp[i+1][j+1]=max(dp[i][j+1],dp[i+1][j])
+    return dp[-1][-1]
+
+print(longestCommonSubsequence("abcde", "ace"))
