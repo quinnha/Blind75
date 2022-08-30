890ac7bf2be1be2cfdbf55e951c787d4093536b7
quinnha <cadetha@gmail.com>
2022-08-30 14:29:20 -0400

    Added Amazon OA Practice

---
+++ Amazon OA Practice/1 - Number Of Islands.py
@@ -0,0 +1,44 @@
+
+def numIslands(grid):
+
+    if not grid: return 0
+
+    count = 0
+    visited = []
+
+    columns = len(grid[0])
+    rows = len(grid)
+
+    for i in range(rows):
+        for j in range(columns):
+            if grid[i][j] == "1":
+                dfs(grid, i,j)
+                count +=1
+
+    return count
+
+def dfs(grid, i, j):
+    if i < 0 or j < 0 or i >=len(grid) or j >= len(grid[0]) or grid[i][j] != "1": return 
+
+    # mark it (visited)
+    grid[i][j] = '#'
+
+    # basically check the square aroudn it
+
+    # check the up down
+    dfs(grid, i+1, j)
+    dfs(grid, i-1, j)
+
+    # check the left right
+    dfs(grid, i, j+1)
+    dfs(grid, i, j-1)
+
+grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","0"]
+]
+
+print(numIslands(grid))
+
---
+++ Amazon OA Practice/README.txt
@@ -0,0 +1,1 @@
+Questions from [here](https://www.teamblind.com/post/amazon-oa-question-complilation-xjQosyo3)
