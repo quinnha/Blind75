d4255cd6ba5fb69b4f74e87b24bfd3e9a4953a9c
Buu Lam Ha <hab8@mcmaster.ca>
2022-10-15 20:09:17 -0400

    Added Valid Parenthesis

---
+++ Amazon OA Practice/7 - Valid Parentheses.py
@@ -0,0 +1,22 @@
+
+def isValid(s):
+
+    stack = []
+
+    open = ["(", "{", "["]
+    close = [")", "}", "]"]
+
+    for ch in s:
+        if ch in open: stack.append(ch)
+        else:
+            if open.index(stack[-1]) == close.index(ch):
+                stack = stack[:-1]
+            else:
+                return False
+    if len(stack) > 0: return False
+
+    return True
+
+print(isValid("()"))
+print(isValid("()[]{}"))
+print(isValid("(]"))
