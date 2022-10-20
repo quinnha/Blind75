ae774192ff7c30ad62a48a1328616f09fbb2f35f
quinnha <cadetha@gmail.com>
2022-10-19 23:12:42 -0400

    Minor spelling changes

--- Extra/10 - Top K Frequent Words.py
+++ Extra/10 - Top K Frequent Words.py
@@ -1,11 +1,12 @@
 from collections import Counter
 
+# counter is a bit weird, maybe avoid? can also use a heap lol
 def topKFrequent(words, k):
     
-    c = Counter(words)                         
+    c = Counter(words)
         
     ans = sorted(c, key = lambda x: (-c[x],x))  
                                                 
     return ans[:k]
 
-print(topKFrequent(["i","love","leetcode","i","love","coding"], 1))
+(topKFrequent(["i","love","leetcode","i","love","coding"], 1))
