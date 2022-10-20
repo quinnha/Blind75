c2f23759aebc3a39e733586c56162448c2f1f197
quinnha <cadetha@gmail.com>
2022-10-20 12:54:56 -0400

    Added unique BST:

---
+++ Google Interview Practice/7 - Unique BST.py
@@ -0,0 +1,17 @@
+from math import factorial
+
+# math way
+def numTrees(n: int) -> int:
+        return factorial(2*n)//factorial(n)//factorial(n)//(n+1)
+
+# dp way
+def numTrees(n: int) -> int:
+        dp = [0] * (n + 1)
+        
+        dp[0], dp[1] = 1, 1
+        
+        for i in range(2, n+1):
+            for j in range(1, i+1):
+                dp[i] += dp[i-j] * dp[j - 1]
+        
+        return dp[n]
