d8dfab34dbfd72e57255496966311466d2bf3f6b
quinnha <cadetha@gmail.com>
2022-10-20 20:10:10 -0400

    Added recursive implementation of level sum

--- Google Interview Practice/10 - Max Level Sum.py
+++ Google Interview Practice/10 - Max Level Sum.py
@@ -1,28 +1,41 @@
-
+from collections import defaultdict
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-class Solution:
-    def maxLevelSum(self, root) -> int:
-        
-        sum, l, ans = float('-inf') , 1, 0
-        q = []
-        q.append(root)
-        
-        while(len(q)):
-            n, s = len(q), 0
 
-            for _ in range(n):
-                temp = q.pop(0)
-                s += temp.val
-                if temp.left: q.append(temp.left)
-                if temp.right: q.append(temp.right)
-            if s > sum:
-                sum = s
-                ans = l
-            l += 1
+# bfs
+def maxLevelSum(root) -> int:
+    
+    sum, l, ans = float('-inf') , 1, 0
+    q = []
+    q.append(root)
+    
+    while(len(q)):
+        n, s = len(q), 0
+
+        for _ in range(n):
+            temp = q.pop(0)
+            s += temp.val
+            if temp.left: q.append(temp.left)
+            if temp.right: q.append(temp.right)
+        if s > sum:
+            sum = s
+            ans = l
+        l += 1
+    
+    return ans
+
+# recursion
+def maxLevelSum(root):
+    levels = defaultdict(int)
+    def sum_vals(root, depth):
+        if root:
+            levels[depth] += root.val
+            sum_vals(root.left,  depth+1)
+            sum_vals(root.right, depth+1)
         
-        return ans
+    sum_vals(root, 1)
+    return max(levels, key=levels.get)
