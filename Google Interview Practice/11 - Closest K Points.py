6b88bb298138707fbeedaa345eeead8d35572473
quinnha <cadetha@gmail.com>
2022-10-22 15:37:59 -0400

    Added non-heap implementation of k points

--- Google Interview Practice/11 - Closest K Points.py
+++ Google Interview Practice/11 - Closest K Points.py
@@ -2,3 +2,9 @@ import heapq
 def kClosest(points, k: int):
         
     return heapq.nsmallest(k, points, lambda x: x[0] ** 2 + x[1] ** 2)
+
+def kClosest2(points, k: int):
+        
+    return sorted(points, key = lambda x: x[0] ** 2 + x[1] ** 2)[:k]
+
+print(kClosest2([[3,3],[5,-1],[-2,4]], 2))
