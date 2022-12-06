278e7601f85a65cdb036c873698f5b51b5a04a69
Buu Lam Ha <hab8@mcmaster.ca>
2022-12-06 18:56:39 -0500

    Added Length of Last Word

---
+++ LC Easies/6 - Length of Last Word.py
@@ -0,0 +1,9 @@
+def lengthOfLastWord(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    split = s.split(" ")
+    while split[-1] == "":
+        split = split[0:-1]
+    return len(split[-1])
