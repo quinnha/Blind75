e0a2bebc2636ed9fe21d427593d24379a10bdb75
quinnha <cadetha@gmail.com>
2022-12-06 12:44:41 -0500

    Edited comment

--- Extra/20 - Remove Nth From End.py
+++ Extra/20 - Remove Nth From End.py
@@ -13,7 +13,7 @@ def removeNthFromEnd(self, head, n):
 
     fast, slow = head, head
     for _ in range(n): fast = fast.next
-    if not fast: return head.next
+    if not fast: return head.next # Crucial check that I forgot
     while fast.next: fast, slow = fast.next, slow.next
     slow.next = slow.next.next
     return head
