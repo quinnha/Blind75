68071814c59d0b6a733adfecf0a7f371e3ef763c
Buu Lam Ha <hab8@mcmaster.ca>
2022-12-06 18:34:16 -0500

    Optimized memory for Letter Combination of Phone

--- Extra/21 - Letter Combination of Phone.py
+++ Extra/21 - Letter Combination of Phone.py
@@ -2,13 +2,10 @@ import itertools
 def letterCombinations(digits):
     if len(digits) == 0: return []
     maps = [[],[],["a", "b", "c"],["d", "e", "f"],["g", "h", "i"],["j", "k", "l"],["m", "n", "o"],["p", "q", "r", "s"],["t", "u", "v"],["w", "x", "y", "z"]]
-    sublist = []
-    for char in digits:
-        sublist.append(maps[int(char)])
-    temp = list(itertools.product(*sublist))
+    temp = list(itertools.product(*[maps[int(char)] for char in digits]))
     out = []
     for combo in temp:
         out.append(''.join(combo))
     return out
 
-print(letterCombinations(""))
+print(letterCombinations("22"))
