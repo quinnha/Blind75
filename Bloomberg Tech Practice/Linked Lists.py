6d029534fba1e22aab3b8377f95648f14c82d518
quinnha <cadetha@gmail.com>
2023-09-22 12:18:01 -0400

    added reorder lsit

--- Bloomberg Tech Practice/Linked Lists.py
+++ Bloomberg Tech Practice/Linked Lists.py
@@ -16,3 +16,72 @@ def reverseList(head):
         head = head.next
 
     return ret
+
+
+# merge sorted lists
+def mergeTwoLists(list1, list2):
+    ret = ListNode()
+    ret1 = ret
+
+    while list1 or list2:
+        if not list1:
+            ret.next = list2
+            list2 = list2.next
+        elif not list2:
+            ret.next = list1
+            list1 = list1.next
+        elif list1.val >= list2.val:
+            ret.next = list2
+            list2 = list2.next
+        elif list2.val > list1.val:
+            ret.next = list1
+            list1 = list1.next
+
+        ret = ret.next
+
+    return ret1.next
+
+
+# reorder list
+def reorderList(head):
+    """
+    Do not return anything, modify head in-place instead.
+    """
+    if not head.next:
+        return head
+
+    forwards = head
+    backwards = None
+
+    helper = head
+
+    n = 0
+
+    while helper:
+        node = ListNode(helper.val)
+        node.next = backwards
+        backwards = node
+        helper = helper.next
+        n += 1
+
+    for i in range(n // 2 - 1):
+        temp = forwards.next
+        forwards.next = backwards
+        backwards = backwards.next
+        forwards = forwards.next
+        forwards.next = temp
+        forwards = forwards.next
+    if n % 2 != 0:
+        temp = forwards.next
+        forwards.next = backwards
+        backwards = backwards.next
+        forwards = forwards.next
+        forwards.next = temp
+        forwards = forwards.next
+    else:
+        forwards = forwards.next
+
+    if forwards:
+        forwards.next = None
+
+    return head
