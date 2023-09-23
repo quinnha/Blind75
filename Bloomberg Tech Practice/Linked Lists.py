54ea0dcc66ad72e86b9e33c159ba46a82e8930da
quinnha <cadetha@gmail.com>
2023-09-22 21:12:50 -0400

    added has cycle

--- Bloomberg Tech Practice/Linked Lists.py
+++ Bloomberg Tech Practice/Linked Lists.py
@@ -137,3 +137,16 @@ def removeNthFromEnd(head, n):
         fast = fast.next
     slow.next = slow.next.next
     return head
+
+
+# has cycle
+def hasCycle(head):
+    try:
+        slow = head
+        fast = head.next
+        while slow is not fast:
+            slow = slow.next
+            fast = fast.next.next
+        return True
+    except:
+        return False
