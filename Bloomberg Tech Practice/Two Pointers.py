2935b1ea1101b59f0009817e198da6bc26675b71
quinnha <cadetha@gmail.com>
2023-09-23 13:17:41 -0400

    added max area

--- Bloomberg Tech Practice/Two Pointers.py
+++ Bloomberg Tech Practice/Two Pointers.py
@@ -43,3 +43,20 @@ def threeSum(nums):
                 mid += 1
                 right -= 1
     return ret
+
+
+# max area
+def maxArea(height):
+    left = 0
+    right = len(height) - 1
+    max_area = 0
+
+    while left < right:
+        max_area = max(max_area, (right - left) * min(height[right], height[left]))
+
+        if height[left] <= height[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return max_area
