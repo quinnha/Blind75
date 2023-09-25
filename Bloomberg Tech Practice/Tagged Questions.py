75cdba81f4786856b99930d4685208bc1ef37b55
quinnha <cadetha@gmail.com>
2023-09-25 15:08:47 -0400

    added number of islands again

--- Bloomberg Tech Practice/Tagged Questions.py
+++ Bloomberg Tech Practice/Tagged Questions.py
@@ -74,3 +74,27 @@ def decodeString(s):
             currString += c
 
     return currString
+
+
+# number of islands (again lol)
+def numIslands(grid):
+    def dfs(grid, i, j):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != "1":
+            return
+
+        grid[i][j] = "#"
+
+        dfs(grid, i + 1, j)
+        dfs(grid, i - 1, j)
+        dfs(grid, i, j + 1)
+        dfs(grid, i, j - 1)
+
+    ret = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == "1":
+                dfs(grid, i, j)
+                ret += 1
+
+    return ret
