973b80cde09d3e7d1563a8895e051eae153ae84b
quinnha <cadetha@gmail.com>
2023-09-25 14:58:57 -0400

    added decode string

--- Bloomberg Tech Practice/Tagged Questions.py
+++ Bloomberg Tech Practice/Tagged Questions.py
@@ -46,3 +46,31 @@ class UndergroundSystem:
         return (
             self.pairs[startStation, endStation] / self.freqs[startStation, endStation]
         )
+
+
+# Decode String
+def decodeString(s):
+    stack = []
+    currNum = 0
+    currString = ""
+
+    for c in s:
+        if c == "[":
+            stack.append(currString)
+            stack.append(currNum)
+            currNum = 0
+            currString = ""
+
+        elif c == "]":
+            num = stack.pop()
+            prevString = stack.pop()
+
+            currString = prevString + num * currString
+
+        elif c.isdigit():
+            currNum = currNum * 10 + int(c)
+
+        else:
+            currString += c
+
+    return currString
